4a8b100ae84fadb7635aa22a3b56474efbfd4409
ouhyeon <163230719+savre01@users.noreply.github.com>
2024-04-03 06:33:52 +0000

    8,9,10 끝

--- lesson-10/main.js
+++ lesson-10/main.js
@@ -1,4 +1,21 @@
 // main.js
 "use strict";
 
+
 // @TODO: 웹 서버를 만들고 실행한다.
+const port = 3000,
+    express = require('express'),
+    layouts = require('express-ejs-layouts'),
+    homeController = require('./controllers/homeController'),
+    app = express();
+
+app.set("port",process.env.PROCESS || port);
+app.set("view engin","ejs");
+
+app.use(layouts);
+
+app.get('/name/:name',homeController.respondWithName2);
+
+app.listen(app.get("port"), () => {
+    console.log(`Express at: http://localhost:${app.get("port")}`);
+});
